52fd6352d1827e510e84a44a50c9ad271628edf7
hanialazreg <lazreghania@gmail.com>
2019-11-09 07:45:33 +0100

    last

--- project.js
+++ project.js
@@ -1,24 +1,9 @@
 $(document).ready(function() {
 	$('.divs').hide()
-	//$('.gridcontainer').hide();
-	//var pass = ['07112019', 'myfriendooo'];
+	
 
-	/*$('#acces').on('click',function(){
-		var idinput = $('#password').val();
-		//console.log(idinput);
-		if (idinput === '07112019'){
-			$('.cv1').show();
-		}
-		else 
-		if(idinput === pass[1]){
-			$('.cv2').show();
-		}
-		
-
-	});
-*/
 	$('#enter').on('click', function(){
-		//var pass = $('')
+		//verify pass word
 		if($('#pass').val() === "hania"){
 		$('.header').toggle("slow");
 		$('.divs').show();
@@ -30,33 +15,24 @@ $(document).ready(function() {
 	});
 	
 	$('#img1').on('click', function(){
+
 		$('#ul1').append('<li>master degree auditing and information security</li>');
 		$('#ul1 li:last').append('<li>bachelor degree information applied in management</li>');
 				
-
+		
 	});
 
 	$("#ul1").click(function(){
- 		 $("#deg").toggle(3000);
+ 		 $("#deg").toggle(2000);
  		 //animate({left: '250px'});
 		}); 
 	$("#info").click(function(){
+		$('#click').hide();
 		$('#work').show();
 	});
-
+	$("#uni").click(function() {
+		$('#vol1').attr('src','amal_ass.jfif');
+	});
 	
-	('#imgexp').on('click', function(){
-	$('imgexp').attr('src','arrow.jpg');
-});
-/*
-$("imgexp").click(
-   function () {
-     $(this).attr('src','work.png');  
-   },
-   function () {
-      $(this).attr('src','arrow.jpg');  
-   }
-);
-*/
 	
 });
